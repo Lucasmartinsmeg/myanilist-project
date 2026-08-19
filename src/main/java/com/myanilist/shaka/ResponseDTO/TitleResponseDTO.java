c141fd5dcda698194142449ac18a891f0a202c7b
Lucas Martins <gregue.bom@gmail.com>
2026-08-18 23:12:58 -0300

    configurando o titleresponse

--- src/main/java/com/myanilist/shaka/ResponseDTO/TitleResponseDTO.java
+++ src/main/java/com/myanilist/shaka/ResponseDTO/TitleResponseDTO.java
@@ -1,5 +1,8 @@
 package com.myanilist.shaka.ResponseDTO;
+import com.myanilist.shaka.Model.Title;
 
 public record TitleResponseDTO(Long id, String title, String image) {
+    public TitleResponseDTO(Title title) {
     this(title.getId(), title.getTitle(), title.getImage());
+    }
 }
