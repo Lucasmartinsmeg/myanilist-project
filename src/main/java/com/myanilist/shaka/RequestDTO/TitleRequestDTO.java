169f58fc018af29c3db721499b80c737eff3fcaa
Lucas Martins <gregue.bom@gmail.com>
2026-08-19 23:23:06 -0300

    atualizando o request e comentando

--- src/main/java/com/myanilist/shaka/RequestDTO/TitleRequestDTO.java
+++ src/main/java/com/myanilist/shaka/RequestDTO/TitleRequestDTO.java
@@ -1,3 +1,11 @@
 package com.myanilist.shaka.RequestDTO;
 import com.myanilist.shaka.Model.Title;
 
+/*Criando um record para receber os dados do titulo e da imagem e
+estou pedindo para que meu construtor recebar o objeto Title,
+Consequentemente, estou chamando o metodo getter que está injetado na minha classe Title*/
+public record TitleRequestDTO(String title, String image){
+    public TitleRequestDTO(Title title){
+        this(title.getTitle(), title.getImage());
+    }
+}
