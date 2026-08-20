2e669c034e949ef6de7c1f893046aecb4cbb0160
Lucas Martins <gregue.bom@gmail.com>
2026-08-19 23:29:41 -0300

    adicionando tradutor de DTO em lista

--- src/main/java/com/myanilist/shaka/Model/Title.java
+++ src/main/java/com/myanilist/shaka/Model/Title.java
@@ -6,17 +6,26 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import com.myanilist.shaka.RequestDTO.TitleRequestDTO;
 
 @Entity(name = "title")
 @Table(name = "title")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Title {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String image;
+
+    public Title(TitleRequestDTO data){
+    this.title = data.title();
+    this.image = data.image();
+    }
 }
+
