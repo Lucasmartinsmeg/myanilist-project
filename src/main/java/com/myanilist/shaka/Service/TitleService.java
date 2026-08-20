d3c815bf4afb1837b8d241a93cb026ed73f39a60
Lucas Martins <gregue.bom@gmail.com>
2026-08-19 22:55:21 -0300

    atualizando service

--- src/main/java/com/myanilist/shaka/Service/TitleService.java
+++ src/main/java/com/myanilist/shaka/Service/TitleService.java
@@ -3,12 +3,13 @@ package com.myanilist.shaka.Service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.myanilist.shaka.Model.Title;
 import com.myanilist.shaka.Repository.TitleRepository;
 
+@Service
 public class TitleService {
-    @Autowired
     private final TitleRepository repository;
 
     public TitleService (TitleRepository repository){
